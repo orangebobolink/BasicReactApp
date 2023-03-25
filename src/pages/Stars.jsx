701643f8458b637f274e62218d489e106c9637a1
Osten Power <osten_power@mail.ru>
2023-03-25 20:02:52 +0300

    change starpage

--- src/pages/Stars.jsx
+++ src/pages/Stars.jsx
@@ -2,13 +2,9 @@ import React, { useState } from "react";
 import { StarRaitingList } from "../components/UI/StarRaiting/StarRaitingList";
 
 export const Stars = () => {
-  const [state, setState] = useState(1);
-
-  const change = (starsSelected) => setState(starsSelected);
-
   return (
     <div>
-      <StarRaitingList starsSelected={state} totalStars={5} onRate={change} />
+      <StarRaitingList />
     </div>
   );
 };
